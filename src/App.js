59a343180298c480ec913c9555fb41b108ef4416
tomast1337 <vycasnicolas@gmail.com>
2022-05-05 02:00:47 -0300

    Adicionando rotas para as novas paginas

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import MenuCliente from './components/Client/menu.js';
 import Carrinho from './components/Client/carrinho';
 import CriarPizza from './components/Client/criar-pizza';
 import CriarUsuario from './components/Client/criar-usuario';
+import MenuAdmin from './components/Admin/menu-admin';
+import GerirPizzas from './components/Admin/gerir-pizzas';
+import GerirIngredientes from './components/Admin/gerir-ingredientes';
 
 function App() {
     return (
@@ -15,6 +18,11 @@ function App() {
             <Route path="/carrinho" element={<Carrinho />} />
             <Route path="/criar-pizza" element={<CriarPizza />} />
             <Route path="/menu" element={<MenuCliente />} />
+
+            <Route path="/menu-admin" element={<MenuAdmin />} />
+            <Route path="/gerir-pizzas" element={<GerirPizzas />} />
+            <Route path="/gerir-ingredientes" element={<GerirIngredientes />} />
+
         </Routes >
     );
 }
