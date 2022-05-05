1e371b78b5b2a1ebf821ca90fc1cf3c424393b50
tomast1337 <vycasnicolas@gmail.com>
2022-05-04 23:44:58 -0300

    Corrigido className

--- src/login.js
+++ src/login.js
@@ -1,30 +1,30 @@
 function Login() {
     return (
         <>
-            <div class="logo text-center">
+            <div className="logo text-center">
                 <h1 >Pizzaria ON</h1>
             </div>
 
-            <div class="container mt-1 mb-5 p-5 bg-transparent">
-                <div class="row">
-                    <h1 class="titulo">Login</h1>
+            <div className="container mt-1 mb-5 p-5 bg-transparent">
+                <div className="row">
+                    <h1 className="titulo">Login</h1>
                 </div>
                 <form>
-                    <div class="row mb-2">
-                        <input type="text" name="campoEmailLog" placeholder="Email" class="form-control" autocomplete="off"
+                    <div className="row mb-2">
+                        <input type="text" name="campoEmailLog" placeholder="Email" className="form-control" autocomplete="off"
                             required>
                         </input>
                     </div>
-                    <div class="row mb-2">
-                        <input type="password" name="campoSenhaLog" placeholder="Senha" class="form-control" required>
+                    <div className="row mb-2">
+                        <input type="password" name="campoSenhaLog" placeholder="Senha" className="form-control" required>
                         </input>
                     </div>
-                    <div class="row mb-5">
-                        <a href="/menu" class="btn btn-primary btn-block ">Logar</a>
+                    <div className="row mb-5">
+                        <a href="/menu" className="btn btn-primary btn-block ">Logar</a>
                     </div>
-                    <div class="row mb-2">
+                    <div className="row mb-2">
                         <p>Não possui conta? Cadastre-se</p>
-                        <a href='Cliente Novo.html' class="btn btn-primary btn-block ">Criar conta</a>
+                        <a href='Cliente Novo.html' className="btn btn-primary btn-block ">Criar conta</a>
                     </div>
                 </form>
             </div>
