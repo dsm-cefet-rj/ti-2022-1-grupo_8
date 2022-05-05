b92762216b1b7fba2d894ad7dfad06a6bf29787f
tomast1337 <vycasnicolas@gmail.com>
2022-05-05 02:00:20 -0300

    Criado Pagina Gerir Pizzas

---
+++ src/components/Admin/gerir-pizzas.js
@@ -0,0 +1,12 @@
+import AdminNav from "./admin-nav";
+import { pizzas } from "../store";
+
+const GerirPizzas = () => {
+    return (
+        <>
+            <AdminNav />
+        </>
+    );
+}
+
+export default GerirPizzas;
