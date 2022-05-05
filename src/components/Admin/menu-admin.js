db7f38a7eef179e97ee63a79dea1cfd276f53ea3
tomast1337 <vycasnicolas@gmail.com>
2022-05-05 01:59:44 -0300

    Criado Pagina Menu admin

---
+++ src/components/Admin/menu-admin.js
@@ -0,0 +1,80 @@
+import AdminNav from "./admin-nav";
+import { pizzas, bebidas, ingredientes } from "../store";
+
+const MenuAdmin = () => {
+    return (
+        <>
+            <AdminNav />
+            <div className="container mb-2 p-1 bg-transparent">
+                <div className="row">
+                    <h1 className="text-center">Relatórios</h1>
+                </div>
+                <div className="row">
+                    <h1 className="text-center">Pizzas Mais Compradas</h1>
+                </div>
+                <div className="row">
+                    <table className="table table-striped table-dark">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Quantidade</th>
+                                <th scope="col">Lucro</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                pizzas.map(pizza => (
+                                    <tr key={pizza.id}>
+                                        <td>{pizza.nome}</td>
+                                        <td>{pizza.quant_comprada}</td>
+                                        <td>{pizza.quant_comprada * 23.37}</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                    <p>Total: R$ {
+                        pizzas.reduce((total, pizza) => {
+                            return total + (pizza.quant_comprada * 23.37);
+                        }, 0)
+                    }</p>
+                </div>
+
+                <div className="row">
+                    <h1 className="text-center">Ingredientes Mais Utilizados</h1>
+                </div>
+                <div className="row">
+                    <table className="table table-striped table-dark">
+                        <thead>
+                            <tr>
+                                <th scope="col">Nome</th>
+                                <th scope="col">Porções</th>
+                                <th scope="col">Custo</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                ingredientes.map(ingrediente => (
+                                    <tr key={ingrediente.id}>
+                                        <td>{ingrediente.nome}</td>
+                                        <td>{ingrediente.usados}</td>
+                                        <td>{ingrediente.usados * ingrediente.preco}</td>
+                                    </tr>
+                                ))
+                            }
+                        </tbody>
+                    </table>
+                    <p>Total: R$ {
+                        ingredientes.reduce((total, ingrediente) => {
+                            return total + (ingrediente.usados * ingrediente.preco);
+                        }, 0)
+                    }</p>
+                </div>
+
+
+            </div>
+        </>
+    );
+}
+
+export default MenuAdmin;
