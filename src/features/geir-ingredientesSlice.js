8266ec85126b8b65ab5c1a0709032c2900c6cb50
tomast1337 <vycasnicolas@gmail.com>
2022-05-07 15:38:01 -0300

    Feature Gerir Ingrediente

---
+++ src/features/geir-ingredientesSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const geirIngredientesSlice = createSlice({
+    name: "geirIngredientes",
+    initialState: {
+        ingredientes: [],
+        idSelecinado: 0,
+    },
+    reducers: {
+        setIngredientes: (state, { payload }) => {
+            state.ingredientes = payload;
+        },
+        setIdSelecinado: (state, { payload }) => {
+            state.idSelecinado = payload;
+        }
+    }
+});
+
+export const { setIngredientes, setIdSelecinado } = geirIngredientesSlice.actions;
+
+const reducer = geirIngredientesSlice.reducer;
+export default reducer;
--- src/features/store.js
+++ src/features/store.js
@@ -1,11 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import carrinhoSlice from "./carrinhoSlice";
 import criarPizzaSlice from "./criar-pizzaSlice";
-
+import geirIngredientesSlice from "./geir-ingredientesSlice";
 
 export default configureStore({
     reducer: {
         carrinho: carrinhoSlice,
         criarPizza: criarPizzaSlice,
+        geirIngredientes: geirIngredientesSlice,
     },
 });
