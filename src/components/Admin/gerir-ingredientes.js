89d4c8d870d627e08fd4a89b74d10057e7fa1830
tomast1337 <vycasnicolas@gmail.com>
2022-05-07 15:39:04 -0300

    Melhorias na pagina Gerir Ingredientes

--- src/components/Admin/gerir-ingredientes.js
+++ src/components/Admin/gerir-ingredientes.js
@@ -1,20 +1,34 @@
 import AdminNav from "./admin-nav";
 import { ingredientes } from "../store";
-
+import { useSelector, useDispatch } from "react-redux";
+import { useState } from "react";
 
 /* 
 Componente: Ingrediente
 Descrição: Componente que renderiza um ingrediente na pagina de gerir ingredientes
 */
 const Ingrediente = (props) => {
-    let { key, imagem, nome, preco } = props;
+    // Dados do ingrediente
+    let { key, imagem, nome, preco } = props.data;
+    // Controle se o ingrediente está selecionado
+    const [selecionado, setSelecionado] = useState(false);
+
+    // Dispatch do Redux
+    const dispatch = useDispatch();
+
+    // Função que seleciona um ingrediente
+    const selecionar = () => {
+        setSelecionado(!selecionado);
+    }
+
+    // Renderização do componente
     return (<>
         <div className="ingrediente">
             <img src={imagem} alt="Pizza" style={{ "width": "100px", }} />
             <br />
             <p>{nome}</p>
             <p>R$ {preco}</p>
-            <a href="#" className="btn btn-primary">Editar</a>
+            <button className="btn btn-primary" onClick={selecionar}>{selecionado ? "Desselecionar" : "Selecionar"}</button>
         </div>
     </>);
 }
@@ -24,6 +38,10 @@ Componente: GerirIngredientes
 Descrição: Componente que renderiza a página de gerenciamento de ingredientes
 */
 const GerirIngredientes = () => {
+    // Dispatch do Redux
+    const dispatch = useDispatch();
+    
+    // Renderização do componente
     return (
         <>
             <AdminNav />
@@ -36,7 +54,7 @@ const GerirIngredientes = () => {
                                 <p><b>Metade 1</b></p>
                                 <div className="scrollmenu">
                                     {ingredientes.map(ingrediente => (
-                                        <Ingrediente key={ingrediente.id} imagem={ingrediente.imagem} nome={ingrediente.nome} preco={ingrediente.preco} />
+                                        <Ingrediente key={ingrediente.id} data={ingrediente} />
                                     ))}
                                 </div>
                             </div>
@@ -50,19 +68,23 @@ const GerirIngredientes = () => {
                     <form action="Administrador Gerir Ingredientes.html" method="post">
                         <div className="form-group mb-2">
                             <label htmlFor="nome">Nome</label>
-                            <input type="text" className="form-control" id="nome" name="nome" placeholder="Nome"/>
+                            <input type="text" className="form-control" id="nome" name="nome" placeholder="Nome" autoComplete="off" />
                         </div>
                         <div className="form-group mb-2">
                             <label htmlFor="preco">Preço</label>
-                            <input type="text" className="form-control" id="preco" name="preco" placeholder="Preço"/>
+                            <input type="number" className="form-control" id="preco" name="preco" placeholder="Preço" autoComplete="off" />
                         </div>
                         <div className="form-group mb-2">
                             <label htmlFor="descricao">Descrição</label>
-                            <input type="text" className="form-control" id="descricao" name="descricao" placeholder="Descrição"/>
+                            <input type="text" className="form-control" id="descricao" name="descricao" placeholder="Descrição" autoComplete="off" />
+                        </div>
+                        <div className="form-group mb-2">
+                            <label htmlFor="descricao">Peso Porção</label>
+                            <input type="number" className="form-control" id="descricao" name="descricao" placeholder="Peso Porção" min="1" max="100" autoComplete="off" />
                         </div>
                         <div className="form-group mb-2">
                             <label htmlFor="imagem">Imagem</label>
-                            <input type="file" className="form-control" id="imagem" name="imagem" placeholder="Imagem"/>
+                            <input type="file" className="form-control" id="imagem" name="imagem" placeholder="Imagem" />
                         </div>
                         <button type="submit" className="btn btn-primary">Adicionar</button>
                     </form>
