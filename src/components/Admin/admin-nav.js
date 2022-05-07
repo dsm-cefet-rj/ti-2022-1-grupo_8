4a05f5c95f2d9f28aad1d1d34aa96f63ef06b41a
tomast1337 <vycasnicolas@gmail.com>
2022-05-07 04:11:32 -0300

    Melhorias no admin-nav

--- src/components/Admin/admin-nav.js
+++ src/components/Admin/admin-nav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 /* 
@@ -6,34 +6,39 @@ Componente: AdminNav
 Descrição: Componente que renderiza o menu de navegação do administrador
 */
 const AdminNav = (props) => {
-    let atual = props.Atual;
+    let atual = props.Atual; /* Página atual */
+    /* Renderização do componente */
+
+    const [collapse, setCollapse] = useState(false); // estado que controla o collapse do menu
+
+    const toggle = () => setCollapse(!collapse); // função que altera o estado do collapse
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark p-2">
             <a className="navbar-brand" href="#">Administrador</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" onClick={toggle}>
                 <span className="navbar-toggler-icon"></span>
             </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
+            <div className={`collapse navbar-collapse ${collapse ? 'show' : ''}`} id="navbarNav">
                 <ul className="navbar-nav">
                     <li className="nav-item">
                         <a className="nav-link" href="/menu-admin">Menu Administrador
                             {
-                                atual === "menu" ? (<span className="sr-only">(Atual)</span>) : null
+                                atual === "menu" ? (<span className="sr-only">(Atual)</span>) : null /* Se a página atual for a página de menu, renderiza um span com a classe sr-only */
                             }
                         </a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="/gerir-ingredientes">Gerir Ingredientes
                             {
-                                atual === "ingredientes" ? (<span className="sr-only">(Atual)</span>) : null
+                                atual === "ingredientes" ? (<span className="sr-only">(Atual)</span>) : null /* Se a página atual for a página de ingredientes, renderiza um span com a classe sr-only */
                             }
                         </a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href="/gerir-pizzas">Gerir Pizzas
                             {
-                                atual === "pizzas" ? (<span className="sr-only">(Atual)</span>) : null
+                                atual === "pizzas" ? (<span className="sr-only">(Atual)</span>) : null /* Se a página atual for a página de pizzas, renderiza um span com a classe sr-only */
                             }
                         </a>
                     </li>
