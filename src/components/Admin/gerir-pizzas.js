c9963ac29eaca5f680b04a6e01dad41553a4e91f
tomast1337 <vycasnicolas@gmail.com>
2022-05-08 16:01:05 -0300

    Adicionado scroll menu na pagina gerir-pizzas

--- src/components/Admin/gerir-pizzas.js
+++ src/components/Admin/gerir-pizzas.js
@@ -1,14 +1,48 @@
 import AdminNav from "./admin-nav";
-import { pizzas } from "../store";
+import { pizzas as pizzaBD } from "../store";
 
 /* 
 Componente: GerirPizzas
 Descrição: Componente que renderiza a página de gerenciamento de pizzas
 */
 const GerirPizzas = () => {
+    const pizzas = pizzaBD.sort((a, b) => {
+        return b.quant_comprada - a.quant_comprada;
+    });
+
     return (
         <>
             <AdminNav />
+            <div class="container mb-2 p-1 bg-transparent">
+                <div class="row section">
+                    <p><b>Pizzas Cadastradas</b></p>
+                    <div class="scrollmenu">
+                        {pizzas.map(pizza => (
+                            <div  style={{ 
+                                "width": "18rem",
+                                "margin": "0.5rem",
+                                "border": "1px solid #ccc",
+                                "border-radius": "0.25rem",
+                                "padding": "1rem",
+                                "display": "flex",
+                                "flex-direction": "column",
+                                "justify-content": "space-between",
+                                "align-items": "center",
+                                }} key={pizza.id}>
+                                <img class="card-img-top" src={pizza.imagem} alt={pizza.nome} style={{ 
+                                    "width": "12rem",
+                                    "height": "12rem",
+                                    "object-fit": "cover",
+                                    }}/>
+                                <div class="card-body">
+                                    <h5 class="card-title">{pizza.nome}</h5>
+                                    <button class="btn btn-lg btn-primary btn-success">Alterar</button>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
