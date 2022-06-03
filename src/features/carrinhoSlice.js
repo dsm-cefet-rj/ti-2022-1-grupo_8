63e553ecc46ee90ff11d6b905cf676539f4724d9
Automated Publisher <actions@users.noreply.github.com>
2022-06-03 16:32:50 +0000

    Automated publish: Fri Jun  3 16:32:50 UTC 2022 48efd474ea986d967e6b991f76514241f5dc95f6

--- src/features/carrinhoSlice.js
+++ src/features/carrinhoSlice.js
@@ -2,67 +2,67 @@ import { createSlice } from "@reduxjs/toolkit";
 
 // recupera os itens do localStorage
 export const getFromLocalStorage = () => {
-    const carrinho = localStorage.getItem("carrinho");
-    if (carrinho) {
-        return JSON.parse(carrinho);
-    }
-    return [];
+  const carrinho = localStorage.getItem("carrinho");
+  if (carrinho) {
+    return JSON.parse(carrinho);
+  }
+  return [];
 };
 
 // salva os itens no localStorage
 export const saveToLocalStorage = (carrinho) => {
-    localStorage.setItem("carrinho", JSON.stringify(carrinho));
+  localStorage.setItem("carrinho", JSON.stringify(carrinho));
 };
 
 const carrinhoSlice = createSlice({
-    name: "carrinho",
-    initialState: {
-        itens: [],
+  name: "carrinho",
+  initialState: {
+    itens: [],
+  },
+  reducers: {
+    // redefine a lista de itens
+    setCarrinho: (state, { payload }) => {
+      state.itens = payload;
+      // salva no localStorage
+      saveToLocalStorage(state.itens);
     },
-    reducers: {
-        // redefine a lista de itens
-        setCarrinho: (state, { payload }) => {
-            state.itens = payload;
-            // salva no localStorage
-            saveToLocalStorage(state.itens);
-        },
-        // adiciona um item ao carrinho
-        adicionarAoCarrinho: (state, { payload }) => {
-            //Carrega o carrinho do localStorage
-            let carrinho = getFromLocalStorage();
-            // Verificar se ja esta no carrinho
-            if (!state.itens.find((item) => item.id === payload.id)) {
-                // Adiciona o item ao carrinho
-                carrinho.push(payload);
-                // Salva no localStorage
-                saveToLocalStorage(carrinho);
-                // Atualiza o estado
-                state.itens = carrinho;
-            }
-        },
-        // remove um item do carrinho
-        removerDoCarrinho: (state, { payload }) => {
-            //Carrega o carrinho do localStorage
-            let carrinho = getFromLocalStorage();
-            // Remove o item do carrinho
-            carrinho = carrinho.filter((item) => item.id !== payload.id);
-            // Salva no localStorage
-            saveToLocalStorage(carrinho);
-            // Atualiza o estado
-            state.itens = carrinho;
-        },
-        // carrega os itens do localStorage
-        carregarCarrinho: (state) => {
-            state.itens = getFromLocalStorage();
-        },
+    // adiciona um item ao carrinho
+    adicionarAoCarrinho: (state, { payload }) => {
+      //Carrega o carrinho do localStorage
+      let carrinho = getFromLocalStorage();
+      // Verificar se ja esta no carrinho
+      if (!state.itens.find((item) => item.id === payload.id)) {
+        // Adiciona o item ao carrinho
+        carrinho.push(payload);
+        // Salva no localStorage
+        saveToLocalStorage(carrinho);
+        // Atualiza o estado
+        state.itens = carrinho;
+      }
     },
+    // remove um item do carrinho
+    removerDoCarrinho: (state, { payload }) => {
+      //Carrega o carrinho do localStorage
+      let carrinho = getFromLocalStorage();
+      // Remove o item do carrinho
+      carrinho = carrinho.filter((item) => item.id !== payload.id);
+      // Salva no localStorage
+      saveToLocalStorage(carrinho);
+      // Atualiza o estado
+      state.itens = carrinho;
+    },
+    // carrega os itens do localStorage
+    carregarCarrinho: (state) => {
+      state.itens = getFromLocalStorage();
+    },
+  },
 });
 
 export const {
-    setCarrinho,
-    adicionarAoCarrinho,
-    removerDoCarrinho,
-    carregarCarrinho,
+  setCarrinho,
+  adicionarAoCarrinho,
+  removerDoCarrinho,
+  carregarCarrinho,
 } = carrinhoSlice.actions;
 
 export const selectCarrinho = (state) => state.carrinho.itens;
