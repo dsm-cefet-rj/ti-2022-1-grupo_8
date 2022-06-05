874601659aef8218e8db0c07bf6b7a049fd8c984
Automated Publisher <actions@users.noreply.github.com>
2022-06-05 17:47:45 +0000

    Automated publish: Sun Jun  5 17:47:45 UTC 2022 d01d4585c5b22dc4638679a77ba74b3527a78f51

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import "./index.scss";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./features/store";
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
