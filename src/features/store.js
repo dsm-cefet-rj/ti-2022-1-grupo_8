d4d4aff2f144525b1d87891de46d09456c6b503b
tomast1337 <vycasnicolas@gmail.com>
2022-06-15 02:46:32 -0300

    Slice novo

--- src/features/store.js
+++ src/features/store.js
@@ -3,6 +3,7 @@ import carrinhoSlice from "./carrinhoSlice";
 import criarPizzaSlice from "./criar-pizzaSlice";
 import gerirIngredientesSlice from "./gerir-ingredientesSlice";
 import ingredientesMetadeSlice from "./ingredientes-metadeSlice";
+import clienteDatabaseSlice from "./clienteDatabaseSlice";
 
 export default configureStore({
     reducer: {
@@ -10,5 +11,6 @@ export default configureStore({
         criarPizza: criarPizzaSlice,
         gerirIngredientes: gerirIngredientesSlice,
         ingredientesMetade: ingredientesMetadeSlice,
+        clienteDatabaseSlice: clienteDatabaseSlice,
     },
 });
