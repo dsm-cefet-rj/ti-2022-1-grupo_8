dd7db20a005d5d0c6cac4becbe652aeeebd4ff1c
tomast1337 <vycasnicolas@gmail.com>
2022-06-15 02:53:10 -0300

    Renomeado funções do DAO

--- backend/usuario.js
+++ backend/usuario.js
@@ -1,19 +1,19 @@
-const { getIngredientes, getPizzas, getProdutos } = require("./data/DAO");
+const { getAllIngredientes, getAllPizzas, getAllProdutos } = require("./data/DAO");
 
 const express = require("express");
 const router = express.Router();
 require("dotenv").config();
 
 router.get("/ingredientes", (req, res) => {
-    res.status(200).json(getIngredientes()).end();
+    res.status(200).json(getAllIngredientes()).end();
 });
 
 router.get("/pizzas", (req, res) => { 
-    res.status(200).json(getPizzas());
+    res.status(200).json(getAllPizzas());
 });
 
 router.get("/produtos", (req, res) => {
-    res.status(200).json(getProdutos());
+    res.status(200).json(getAllProdutos());
 });
 
 module.exports = router;
