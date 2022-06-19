d06c9c6dccb267d460ef090b252c5272b2a6c8f9
tomast1337 <vycasnicolas@gmail.com>
2022-06-18 23:17:09 -0300

    Adicionado pagina NotFound

---
+++ src/components/geral/not-found.js
@@ -0,0 +1,13 @@
+import React from "react";
+import style from "./not-found.module.scss";
+
+const NotFound = () => {
+    return (
+        <div className={style.notFound}>
+            <h1>NEM UMA PIZZA POR AQUI!</h1>
+            <a href="/">Voltar para o início 🍕 😋</a>
+        </div>
+    );
+}
+
+export default NotFound;
