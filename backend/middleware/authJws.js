f978482ba30b2ed4f435e12394eda3e7bc29ad74
tomast1337 <vycasnicolas@gmail.com>
2022-06-19 21:51:00 -0300

    Tratamento para se o token of undefined

--- backend/middleware/authJws.js
+++ backend/middleware/authJws.js
@@ -5,17 +5,17 @@ require("dotenv").config();
 const jwt_secret = process.env.JWT_SECRET;
 // verifica se o token está válido e se o usuário é o mesmo que o token
 verificarToken = (req, res, next) => {
-    const token = (
-        req.headers["x-access-token"] ||
+    let token = req.headers["x-access-token"] ||
         req.headers["authorization"] ||
-        req.headers["x-auth-token"]
-    ).replace("Bearer ", ""); // O token é cortado do prefixo do Bearer.
+        req.headers["x-auth-token"];
+
     if (!token) {
         // se não existir token
         return res
             .status(401)
             .send({ auth: false, message: "No token provided." });
     } else {
+        token = token.replace("Bearer ", ""); // remove o Bearer do token
         jwt.verify(token, jwt_secret, (err, decoded) => {
             if (err) {
                 // se o token estiver inválido
@@ -38,16 +38,18 @@ verificarToken = (req, res, next) => {
 
 // verifica se o usuário é admin
 isAdmin = (req, res, next) => {
-    const token = (
-        req.headers["x-access-token"] ||
+    let token = req.headers["x-access-token"] ||
         req.headers["authorization"] ||
-        req.headers["x-auth-token"]
-    ).replace("Bearer ", "");
-    const tokenPayload = jwt.decode(token); // decodifica o token. Conteúdo: email, type, iat, exp
-    const type = tokenPayload.type;
-    if (type == "admin") {
-        next();
-    } 
+        req.headers["x-auth-token"];
+    if (token) {
+        token = token.replace("Bearer ", ""); // remove o Bearer do token
+        const tokenPayload = jwt.decode(token); // decodifica o token. Conteúdo: email, type, iat, exp
+        const type = tokenPayload.type;
+        if (type == "admin") {
+            next();
+        }
+    }
+
     return res.status(401).json({ // Usuário não é admin 401
         auth: false,
         message: "Acesso negado",
@@ -56,16 +58,17 @@ isAdmin = (req, res, next) => {
 
 // verifica se o usuário é funcionário
 isFuncionario = (req, res, next) => {
-    const token = (
-        req.headers["x-access-token"] ||
+    let token = req.headers["x-access-token"] ||
         req.headers["authorization"] ||
         req.headers["x-auth-token"]
-    ).replace("Bearer ", "");
-    const tokenPayload = jwt.decode(token); // decodifica o token. Conteúdo: email, type, iat, exp
-    const type = tokenPayload.type;
-    if (type == "funcionario") {
-        next();
-    } 
+    if (token) {
+        token = token.replace("Bearer ", ""); // remove o Bearer do token
+        const tokenPayload = jwt.decode(token); // decodifica o token. Conteúdo: email, type, iat, exp
+        const type = tokenPayload.type;
+        if (type == "funcionario") {
+            next();
+        }
+    }
     return res.status(401).json({ // Usuário não é admin 401
         auth: false,
         message: "Acesso negado",
