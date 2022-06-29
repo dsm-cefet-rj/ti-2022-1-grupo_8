9e4b6408e871d5086cdbe498fb39f289b9782f96
Dayvison <dayvisonaoc@gmail.com>
2022-06-29 00:18:25 -0300

    Atualizando menu funcionário

--- src/components/funcionario/menuFuncionario.js
+++ src/components/funcionario/menuFuncionario.js
@@ -13,16 +13,37 @@ import {
 } from "../../features/pedidos-funcionarioSlice";
 
 const converterData = (data) => {
-    return "2022-04-05";
+    if (!data) return "Sem Data";
+    // Converte de unix timestamp para data
+    let dataFormatada = new Date(data);
+    let dia = dataFormatada.getDate();
+    let mes = dataFormatada.getMonth() + 1;
+    let ano = dataFormatada.getFullYear();
+    let hora = dataFormatada.getHours();
+    let minuto = dataFormatada.getMinutes();
+
+    hora = hora < 10 ? "0" + hora : hora; // Adiciona zero à esquerda
+    minuto = minuto < 10 ? "0" + minuto : minuto; // Adiciona zero à esquerda
+    // Formata a data 
+    return `${dia}/${mes}/${ano} ${hora}:${minuto}`;
 };
 
 const PedidoCard = (props) => {
     const idPedido = props.id;
     const email = props.email;
-    const data = props.dataHora;
+    const data = props.data;
     const endereco = props.endereco;
     const itens = props.itens;
     const status = props.status;
+
+    const avançarPedido = () => {
+        let funções = {
+            "Feitos": () => { },
+            "Em andamento": () => { },
+            "Concluídos": () => { },
+        }
+        funções[status]();
+    }
 
     return (
         <>
@@ -44,18 +65,25 @@ const PedidoCard = (props) => {
                         <ul className="list-group list-group-flush">
                             {itens.map((item, index) => (
                                 <li key={index} className="list-group-item">
-                                    <strong>{item.quantidade}x </strong>
+                                    <strong
+                                        key={index}>{item.quantidade}x </strong>
                                     {item.nome}
                                 </li>
                             ))}
                         </ul>
                     </div>
                 </ul>
-                <div className="card-footer">
-                    <button className="btn btn-primary float-end">
-                        Avançar &#62;
-                    </button>
-                </div>
+                {"Concluído" === status ? (
+                    <div className="card-footer">
+                        {"Concluído"}
+                    </div>
+                ) : (
+                    <div className="card-footer">
+                        <button className="btn btn-primary float-end">
+                            {"Avançar >"}
+                        </button>
+                    </div>
+                )}
             </div>
         </>
     );
@@ -86,55 +114,19 @@ const MenuFuncionario = () => {
         dispatch(fetchPedidosConcluidos());
     }, [dispatch]);
 
-    //const pedidosFeitos = useSelector(selectPedidosFeitos);
-    //const pedidosEmAndamento = useSelector(selectPedidosEmAndamento);
-    //const pedidosConcluidos = useSelector(selectPedidosConcluidos);
+    const pedidosFeitos = useSelector(selectPedidosFeitos);
+    const pedidosEmAndamento = useSelector(selectPedidosEmAndamento);
+    const pedidosConcluidos = useSelector(selectPedidosConcluidos);
 
-    //const pedidosFeitos = useSelector(
-    //    (state) => state.pedidosFuncionario.pedidosFeitos
-    //);
-    //const pedidosEmAndamento = useSelector(
-    //    (state) => state.pedidosFuncionario.pedidosEmAndamento
-    //);
-    //const pedidosConcluidos = useSelector(
-    //    (state) => state.pedidosFuncionario.pedidosConcluidos
-    //);
-    //
-    //const pedidosFeitosRef = useRef(pedidosFeitos);
-    //const pedidosEmAndamentoRef = useRef(pedidosEmAndamento);
-    //const pedidosConcluidosRef = useRef(pedidosConcluidos);
-    //
-    //useEffect(() => {
-    //    pedidosFeitosRef.current = pedidosFeitos;
-    //    pedidosEmAndamentoRef.current = pedidosEmAndamento;
-    //    pedidosConcluidosRef.current = pedidosConcluidos;
-    //}, [pedidosFeitos, pedidosEmAndamento, pedidosConcluidos]);
+    const pedidosFeitosRef = useRef(pedidosFeitos);
+    const pedidosEmAndamentoRef = useRef(pedidosEmAndamento);
+    const pedidosConcluidosRef = useRef(pedidosConcluidos);
 
-    const pedidosFeitos = [
-        {
-            id: 4,
-            email: "bernardocosta@gmail.com",
-            dataHora: 1655929109,
-            endereco: "Av. Maracanã, 123",
-            carrinho: [
-                {
-                    id: 1,
-                    nome: "Pizza Grande de Frango com Requeijão",
-                    quantidade: 2,
-                },
-                {
-                    id: 2,
-                    nome: "Refrigerante",
-                    quantidade: 5,
-                },
-                {
-                    id: 5,
-                    nome: "Pudim de leite",
-                    quantidade: 2,
-                },
-            ],
-        },
-    ];
+    useEffect(() => {
+        pedidosFeitosRef.current = pedidosFeitos;
+        pedidosEmAndamentoRef.current = pedidosEmAndamento;
+        pedidosConcluidosRef.current = pedidosConcluidos;
+    }, [pedidosFeitos, pedidosEmAndamento, pedidosConcluidos]);
 
     return (
         <>
@@ -164,10 +156,42 @@ const MenuFuncionario = () => {
 
                     <div className="col-md-4">
                         <Header titulo="Em andamento" cor="warning"></Header>
+                        {pedidosEmAndamento.length > 0 ? (
+                            pedidosEmAndamento.map((pedido) => (
+                                <PedidoCard
+                                    id={pedido.id}
+                                    email={pedido.email}
+                                    data={pedido.dataHora}
+                                    endereco={pedido.endereco}
+                                    itens={pedido.carrinho}
+                                    status={pedido.status}
+                                />
+                            ))
+                        ) : (
+                            <p className="text-center">
+                                Nenhum pedido em andamento!
+                            </p>
+                        )}
                     </div>
 
                     <div className="col-md-4">
                         <Header titulo="Concluídos" cor="success"></Header>
+                        {pedidosConcluidos.length > 0 ? (
+                            pedidosConcluidos.map((pedido) => (
+                                <PedidoCard
+                                    id={pedido.id}
+                                    email={pedido.email}
+                                    data={pedido.dataHora}
+                                    endereco={pedido.endereco}
+                                    itens={pedido.carrinho}
+                                    status={pedido.status}
+                                />
+                            ))
+                        ) : (
+                            <p className="text-center">
+                                Nenhum pedido em concluídos!
+                            </p>
+                        )}
                     </div>
                 </div>
             </div>
