8cb94dc7a3c9f3659d97dd13d85ccb5474e4caa2
tomast1337 <vycasnicolas@gmail.com>
2022-06-30 05:33:44 -0300

    Criando arquivo para algoritmos de uso geral

---
+++ src/components/geral/util.js
@@ -0,0 +1,16 @@
+export const converterData = (data) => {
+    if (!data) return "Sem Data";
+    // Converte de unix timestamp para data
+    let dataFormatada = new Date(data);
+    let dia = dataFormatada.getDate();
+    let mes = dataFormatada.getMonth() + 1;
+    let ano = dataFormatada.getFullYear();
+    let hora = dataFormatada.getHours();
+    let minuto = dataFormatada.getMinutes();
+
+    hora = hora < 10 ? "0" + hora : hora; // Adiciona zero à esquerda
+    minuto = minuto < 10 ? "0" + minuto : minuto; // Adiciona zero à esquerda
+    // Formata a data
+    return `${dia}/${mes}/${ano} ${hora}:${minuto}`;
+};
+
