c1ad54c4526b4e1001e66e4a78f405e741997804
tomast1337 <vycasnicolas@gmail.com>
2022-06-30 05:34:31 -0300

    Codigo componente Pedido Card para arquivo separa e adicionando uso mais geral para o compnete

---
+++ src/components/geral/pedido-card.js
@@ -0,0 +1,99 @@
+import { getSessionFromLocalStorage } from "../../features/sessionSlice";
+import { converterData } from "./util";
+
+export const PedidoCard = (props) => {
+    const idPedido = props.id;
+    const email = props.email;
+    const data = props.data;
+    const endereco = props.endereco;
+    const itens = props.itens;
+    const status = props.status;
+    const context = props.context;
+
+    const avançarPedido = () => {
+        const token = getSessionFromLocalStorage();
+        let funções = {
+            Feito: () => {
+                const url = `http://localhost:3001/funcionario/iniciar-pedido/${idPedido}`;
+                fetch(url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        authorization: `Bearer ${token}`,
+                    },
+                }).then((response) => {
+                    if (response.status === 200) {
+                        console.log("Pedido iniciado");
+                    }
+                });
+            },
+            "Em andamento": () => {
+                const url = `http://localhost:3001/funcionario/finalizar-pedido/${idPedido}`;
+                fetch(url, {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                        authorization: `Bearer ${token}`,
+                    },
+                }).then((response) => {
+                    if (response.status === 200) {
+                        console.log("Pedido iniciado");
+                    }
+                });
+            },
+            Concluídos: () => {
+                console.log(":) Pedido concluído");
+            },
+        };
+
+        funções[status]();
+        window.location.reload();
+    };
+
+    return (
+        <>
+            <div className="card" style={{ width: "18rem", lineHeight: "1" }}>
+                <div className="card-header text-center">
+                    <h5 className="card-title" style={{ fontSize: "1.15rem" }}>
+                        Pedido #{idPedido}
+                    </h5>
+                    <p style={{ marginBottom: 0 }}>{converterData(data)}</p>
+                </div>
+                <div className="card-body">
+                    <p className="card-text">
+                        <strong>E-mail:</strong> {email}
+                    </p>
+                    <p className="card-text">
+                        <strong>Endereço:</strong> {endereco}
+                    </p>
+                </div>
+                <ul className="list-group list-group-flush">
+                    <div className="card-body">
+                        <ul className="list-group list-group-flush">
+                            {itens.map((item, index) => (
+                                <li key={index} className="list-group-item">
+                                    <strong key={index}>
+                                        {item.quantidade}x{" "}
+                                    </strong>
+                                    {item.nome}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </ul>
+                {"Concluído" === status && context !== "gerir-usuarios"? (
+                    <div className="card-footer">{"Concluído"}</div>
+                ) : (
+                    <div className="card-footer">
+                        <button
+                            className="btn btn-primary float-end"
+                            onClick={avançarPedido}
+                        >
+                            {"Avançar >"}
+                        </button>
+                    </div>
+                )}
+            </div>
+        </>
+    );
+};
