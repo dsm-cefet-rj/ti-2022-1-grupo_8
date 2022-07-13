03f294b7327b710f84832d57f4d50160f3e8fdf8
Bentroen <bemcdc@gmail.com>
2022-07-13 03:45:51 -0300

    Adicionar funcionalidade de gerir pizzas

--- src/components/Admin/gerir-pizzas.js
+++ src/components/Admin/gerir-pizzas.js
@@ -8,9 +8,16 @@ import {
     fetchIngredientes,
 } from "../../features/clienteDatabaseSlice";
 import {
-    removeIngrediente,
+    setIdSelecionado,
+    setNome,
+    setDescricao,
+    setIngredientes,
     addIngrediente,
-    selectGerirPizza,
+    removeIngrediente,
+    selectIdSelecionado,
+    selectNome,
+    selectDescricao,
+    selectIngredientesPizza,
 } from "../../features/gerir-pizzaSlice";
 import AdminNav from "./admin-nav";
 import styles from "./gerir-pizzas.module.scss";
@@ -19,68 +26,159 @@ import { getSessionFromLocalStorage } from "../../features/sessionSlice";
 Componente: GerirPizzas
 Descrição: Componente que renderiza a página de gerenciamento de pizzas
 */
-const GerirPizzas = () => {
+
+const Pizza = (props) => {
+    // Dispatch do Redux
     const dispatch = useDispatch();
 
-    const pizzas = useSelector(selectPizzas);
-    const ingredientes = useSelector(selectIngredientes);
+    // Variavies que controlam os ingredientes do banco de dados.
+    const pizzasBD = useSelector(selectPizzas);
 
-    const [erro, setErro] = useState("");
+    // Dados do ingrediente
+    const { id, imagem, nome, descricao, ingredientes } = props.data;
+    // Controla se a pizza está selecionada
+    const [selecionado, setSelecionado] = useState(false);
 
-    // Variáveis que controlam o nome da pizza
-    const [nome, setNome] = useState("");
-    // Variáveis que controlam o arquivo de imagem da pizza.
+    // Função que seleciona uma pizza
+    const selecionar = () => {
+        setSelecionado(!selecionado);
+        if (!selecionado) {
+            dispatch(
+                setIdSelecionado({
+                    id: id,
+                })
+            );
+            dispatch(
+                setNome({
+                    nome: nome,
+                })
+            );
+            dispatch(
+                setDescricao({
+                    descricao: descricao,
+                })
+            );
+            dispatch(
+                setIngredientes({
+                    ingredientes: ingredientes,
+                })
+            );
+            document.getElementById("form-pizza").scrollIntoView({
+                behavior: "instant",
+                block: "center",
+            });
+            //desselecionar todas as outras pizzas
+            pizzasBD.forEach((pizza) => {
+                if (pizza.id !== id) {
+                    let elem = document.getElementById(`pizza-${pizza.id}`);
+                    elem.innerHTML = "Selecionar";
+                }
+            });
+        } else {
+            dispatch(
+                setIdSelecionado({
+                    id: 0,
+                })
+            );
+            dispatch(
+                setNome({
+                    nome: "",
+                })
+            );
+            dispatch(
+                setDescricao({
+                    descricao: "",
+                })
+            );
+            dispatch(
+                setIngredientes({
+                    ingredientes: ingredientes,
+                })
+            );
+        }
+    };
+
+    useEffect(() => {
+        dispatch(fetchIngredientes());
+    }, [dispatch]);
+
+    // Renderização do componente
+    return (
+        <>
+            <div className="pizza">
+                <img src={imagem} alt="Pizza" style={{ width: "100px" }} />
+                <br />
+                <p>{nome}</p>
+                <button
+                    className="btn btn-primary"
+                    onClick={selecionar}
+                    id={`pizza-${id}`}
+                >
+                    {selecionado ? "Desselecionar" : "Selecionar"}
+                </button>
+            </div>
+        </>
+    );
+};
+
+const GerirPizzas = () => {
+    // Dispatch do Redux
+    const dispatch = useDispatch();
+
+    // Variavies que controlam os ingredientes do banco de dados.
+    const pizzasBD = useSelector(selectPizzas);
+    const ingredientesBD = useSelector(selectIngredientes);
+
+    const idSelecionado = useSelector(selectIdSelecionado);
+    const nome = useSelector(selectNome);
+    const descricao = useSelector(selectDescricao);
+    const ingredientes = useSelector(selectIngredientesPizza);
     const [imagem, setImagem] = useState("");
-    // Variáveis que controlam a descrição da pizza.
-    const [descricao, setDescricao] = useState("");
-    // Variáveis que controlam os ingredientes selecionados.
-    const ingrediente = useSelector(selectGerirPizza).ingrediente; // ingredientes
 
-    const [idSelecinado, setIdSelecionado] = useState("");
+    useEffect(() => {
+        dispatch(fetchPizzas());
+        dispatch(fetchIngredientes());
+    }, [dispatch]);
 
-    const [preco, setPreco] = useState(0);
-    const [editando, setEditando] = useState(false);
+    useEffect(() => {
+        if (idSelecionado !== 0) {
+            document.getElementById("imagem-field").hidden = true;
+        } else {
+            document.getElementById("imagem-field").hidden = false;
+        }
+    }, [idSelecionado]);
 
-    const handleSubmit = async (e) => {
+    const handleButton = async (e) => {
         e.preventDefault();
-        let form_data = new FormData();
-        form_data.append("nome", nome);
-        form_data.append("descricao", descricao);
-        form_data.append("imagem", imagem);
-        form_data.append("id", idSelecinado);
-
+        let pizza = {
+            nome: nome,
+            descricao: descricao,
+            imagem: imagem,
+            ingredientes: ingredientes,
+            _id: idSelecionado,
+        };
         const form = new FormData();
-        for (const key in pizza) {
+        for (let key in pizza) {
             form.append(key, pizza[key]);
         }
-
         const token = getSessionFromLocalStorage();
+
         const request = {
             method: "POST",
             url: "http://localhost:3001/admin/editar-pizza",
             headers: {
-                "Content-Type": "multipart/form-data",
                 "x-access-token": `Bearer ${token}`,
             },
-            data: form_data,
-            onUploadProgress: (progressEvent) => {
-                const percentCompleted = Math.round(
-                    (progressEvent.loaded * 100) / progressEvent.total
-                );
-                console.log(percentCompleted);
-            },
+            data: form,
         };
 
-        axios(request)
-            .then((response) => {
-                if (response.status === 200) {
-                    dispatch(fetchIngredientes());
-                    console.log("Pizza editada com sucesso!");
-                } else console.log("Erro ao editar pizza!");
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+        const response = await axios(request);
+        if (response.status === 200) {
+            // reload window
+            window.location.reload();
+        } else {
+            console.log(response.data.error);
+        }
     };
 
     const handleCheckbox = (e) => {
@@ -109,7 +207,7 @@ const GerirPizzas = () => {
                     <div className="row m-1 section">
                         {/*Area de seleção de pizza existente*/}
                         <div className="scrollmenu">
-                            {pizzas.map((pizza) => (
+                            {pizzasBD.map((pizza) => (
                                 <div
                                     style={{
                                         width: "18rem",
@@ -143,7 +241,6 @@ const GerirPizzas = () => {
                                             className="btn btn-lg btn-primary btn-success"
                                             id={`pizza-${pizza.id}`}
                                             onClick={() => {
-                                                setEditando(true);
                                                 setNome(pizza.nome);
                                                 setDescricao(pizza.descricao);
                                                 setIdSelecionado(pizza.id);
@@ -174,7 +271,7 @@ const GerirPizzas = () => {
                                                     elem.innerHTML = "Editar";
                                                 }
 
-                                                pizzas.map((pizzaOBJ) => {
+                                                pizzasBD.map((pizzaOBJ) => {
                                                     if (
                                                         pizzaOBJ.id !== pizza.id
                                                     ) {
@@ -203,47 +300,71 @@ const GerirPizzas = () => {
                     </div>
                     {/*Formulário de cadastro e alteração de pizza de pizza*/}
                     <div className="row m-1 section">
-                        <form>
+                        <form
+                            action="Administrador Gerir Pizzas.html"
+                            method="post"
+                            id="form-pizza"
+                        >
                             {/* Nome*/}
                             <div className="form-group mb-2">
                                 <label htmlFor="nome">Nome</label>
                                 <input
-                                    id="nome"
-                                    className="form-control"
                                     type="text"
+                                    className="form-control"
+                                    id="nome"
+                                    name="nome"
+                                    placeholder="Nome"
+                                    autoComplete="off"
+                                    onChange={(e) =>
+                                        dispatch(
+                                            setNome({
+                                                nome: e.target.value,
+                                            })
+                                        )
+                                    }
                                     value={nome}
-                                    onChange={(e) => setNome(e.target.value)}
                                 />
                             </div>
                             {/* Descrição*/}
                             <div className="form-group mb-2">
                                 <label htmlFor="descricao">Descrição</label>
                                 <textarea
-                                    id="descricao"
-                                    className="form-control"
                                     type="text"
-                                    value={descricao}
+                                    className="form-control"
+                                    id="descricao"
+                                    name="descricao"
+                                    placeholder="Descrição"
+                                    autoComplete="off"
                                     onChange={(e) =>
-                                        setDescricao(e.target.value)
+                                        dispatch(
+                                            setDescricao({
+                                                descricao: e.target.value,
+                                            })
+                                        )
                                     }
+                                    value={descricao}
                                 />
                             </div>
                             {/* Imagem */}
-                            <div className="form-group mb-2">
+                            <div className="form-group mb-2" id="imagem-field">
                                 <label htmlFor="imagem">Imagem</label>
                                 <input
-                                    id="imagem"
-                                    className="form-control"
                                     type="file"
+                                    className="form-control"
+                                    id="imagem"
+                                    name="imagem"
+                                    placeholder="Imagem"
                                     onChange={(e) => {
-                                        const [file] = e.target.files;
+                                        // setImagem to the fist file uploaded
+                                        let [file] = e.target.files;
+
                                         setImagem(file);
                                     }}
                                 />
                             </div>
                             {/* Ingredientes*/}
                             <div className="scrollmenu">
-                                {ingredientes.map((ingrediente) => (
+                                {ingredientesBD.map((ingrediente) => (
                                     <div
                                         className="ingrediente"
                                         key={ingrediente.id}
@@ -289,7 +410,7 @@ const GerirPizzas = () => {
                             }}
                         >
                             <h3>Preço total:</h3>
-                            <h3>{"R$: " + preco.toFixed(2)}</h3>
+                            <h3>{"R$: " + "10"}</h3>
                         </div>
                     </div>
                     <div className="row section">
@@ -297,7 +418,7 @@ const GerirPizzas = () => {
                         <div className="col-md-12">
                             <button
                                 className="btn btn-lg btn-success"
-                                onClick={handleSubmit}
+                                onClick={handleButton}
                             >
                                 Confirmar
                             </button>
--- src/features/gerir-pizzaSlice.js
+++ src/features/gerir-pizzaSlice.js
@@ -3,21 +3,47 @@ import { createSlice } from "@reduxjs/toolkit";
 const gerirPizzaSlice = createSlice({
     name: "gerirPizza",
     initialState: {
-        ingrediente: [],
+        idSelecionado: 0,
+        nome: "",
+        descricao: "",
+        ingredientes: [],
     },
     reducers: {
+        setIdSelecionado: (state, action) => {
+            state.idSelecionado = action.payload;
+        },
+        setNome: (state, action) => {
+            state.nome = action.payload;
+        },
+        setDescricao: (state, action) => {
+            state.descricao = action.payload;
+        },
+        setIngredientes: (state, action) => {
+            state.ingredientes = action.payload;
+        },
         addIngrediente: (state, action) => {
-            state.ingrediente.push(action.payload);
+            state.ingredientes.push(action.payload);
         },
         removeIngrediente: (state, action) => {
-            state.ingrediente.splice(action.payload, 1);
+            state.ingredientes.splice(action.payload, 1);
         },
     },
 });
 
-export const { removeIngrediente, addIngrediente } = gerirPizzaSlice.actions;
+export const {
+    setIdSelecionado,
+    setNome,
+    setDescricao,
+    setIngredientes,
+    addIngrediente,
+    removeIngrediente,
+} = gerirPizzaSlice.actions;
 
-export const selectGerirPizza = (state) => state.gerirPizza;
+// para ser usado com o UseSelector
+export const selectIdSelecionado = (state) => state.gerirPizza.idSelecionado;
+export const selectNome = (state) => state.gerirPizza.nome;
+export const selectDescricao = (state) => state.gerirPizza.descricao;
+export const selectIngredientesPizza = (state) => state.gerirPizza.ingredientes;
 
 const reducer = gerirPizzaSlice.reducer;
 export default reducer;
