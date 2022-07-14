7816c3bc16fe07ba4ef653f72adacd1804a14448
Automated Publisher <actions@users.noreply.github.com>
2022-07-14 02:20:51 +0000

    Automated publish: Thu Jul 14 02:20:51 UTC 2022 947753512111686f9dd1df82d784b86ff46cd9d7

--- backend/data/DaoPizza.js
+++ backend/data/DaoPizza.js
@@ -23,9 +23,7 @@ const validaçãoPedido = (pizza) => {
             throw new Error(`${key} é um campo obrigatório`);
         }
         if (typeof pizza[key] !== PizzaValidTypes[key]) {
-            throw new Error(
-                `${key} deve ser do tipo ${PizzaValidTypes[key]}`
-            );
+            throw new Error(`${key} deve ser do tipo ${PizzaValidTypes[key]}`);
         }
     });
     if (pizza.id) delete pizza.id;
@@ -64,7 +62,7 @@ const addPizza = async (pizzas) => {
 const editPizza = async (id, pizza) => {
     const connection = await getConnection(); // conectar ao banco de dados
     validaçãoPedido(pizza);
-    if(pizza._id) delete pizza._id;
+    if (pizza._id) delete pizza._id;
     await connection
         .collection("pizzas")
         .updateOne({ _id: new ObjectId(id) }, { $set: pizza });
